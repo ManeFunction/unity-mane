641eb91519faa658b9518bca03bb74ec0b7bddc0
Mane Function <mfunc@outlook.com>
2020-03-12 12:24:44 +0300

    Added Vector2.Project method

--- Extensions/VectorExtensions.cs
+++ Extensions/VectorExtensions.cs
@@ -141,5 +141,17 @@ namespace Mane.Extensions
 
             return v;
         }
+        
+        public static Vector2 Project(this Vector2 vector, Vector2 onNormal)
+        {
+            float num1 = Vector2.Dot(onNormal, onNormal);
+            if (num1 < Mathf.Epsilon)
+            {
+                return Vector2.zero;
+            }
+            float num2 = Vector2.Dot(vector, onNormal);
+            
+            return new Vector2(onNormal.x * num2 / num1, onNormal.y * num2 / num1);
+        }
     }
 }
