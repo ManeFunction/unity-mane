63ab9c70a1d30046f09151f969b992b6702b142b
Mane Function <mfunc@outlook.com>
2020-07-23 14:25:58 +0300

    1.2.1: Added fully-functional Layer property drawer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,1 @@
-Added Average() extension for the Vectors collections.
-Removed all C# 7.0+ expressions to make the code compatible with earlier version
-(full 7.0 translation will be applied later with Unity 2018 support drop) 
+Added fully-functional Layer property drawer.
---
+++ Editor/PropertyDrawers/LayerPropertyDrawer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Mane.Extensions.Editor
+{
+    [CustomPropertyDrawer(typeof(LayerAttribute))]
+    public class LayerPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.intValue = EditorGUI.LayerField(position, label, property.intValue);
+        }
+    }
+}
