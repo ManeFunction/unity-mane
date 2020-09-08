f68e3b56b4771afbda861fbbb5da9709b103d3b5
Mane Function <mfunc@outlook.com>
2020-09-08 17:11:53 +0300

    1.2.8: Added Postfix attribute (Property Drawer)

---
+++ Editor/PropertyDrawers/PostfixPropertyDrawer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Mane.Extensions.Editor
+{
+    [CustomPropertyDrawer(typeof(PostfixAttribute))]
+    public class PostfixPropertyDrawer : PropertyDrawer
+    {
+        private float _postfixWidth = -1f;
+        private GUIStyle _postfixStyle = null;
+
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            PostfixAttribute attr = attribute as PostfixAttribute;
+
+            if (_postfixWidth == -1f)
+            {
+                if (attr.Width == 0f)
+                {
+                    _postfixWidth = GUI.skin.label.CalcSize(new GUIContent(attr.Text)).x;
+                }
+                else
+                {
+                    _postfixWidth = attr.Width;
+                }
+            }
+
+            if (_postfixStyle == null)
+            {
+                _postfixStyle = new GUIStyle(GUI.skin.label)
+                {
+                    alignment = TextAnchor.MiddleRight
+                };
+            }
+
+            Rect fieldRect = position;
+            fieldRect.width -= _postfixWidth;
+            EditorGUI.PropertyField(fieldRect, property, label, true);
+
+            Rect postfixRect = position;
+            EditorGUI.LabelField(postfixRect, attr.Text, _postfixStyle);
+        }
+    }
+}
---
+++ Runtime/Attributes/PostfixAttribute.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Mane.Extensions
+{
+    public class PostfixAttribute : PropertyAttribute
+    {
+        public string Text { get; private set; }
+        public float Width { get; private set; }
+
+        public PostfixAttribute(string text, float width = 0f)
+        {
+            Text = text;
+            Width = width;
+        }
+    }
+}
