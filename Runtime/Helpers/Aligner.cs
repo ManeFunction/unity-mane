fc7b65dde5f61b48da6460b93acf0279dcdfd301
Mane Function <mfunc@outlook.com>
2020-09-30 14:08:45 +0300

    1.3.8: Added Aligner component to help you align item stacks with constant offset

---
+++ Runtime/Helpers/Aligner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Mane.Extensions
+{
+    public class Aligner : MonoBehaviour
+    {
+        [SerializeField] private float _margin = 1f;
+        [SerializeField] private Vector3 _axis = Vector3.right;
+        [SerializeField] private Transform[] _items;
+
+        private void OnValidate()
+        {
+            Vector3 step = _axis * _margin;
+            Vector3 startingPosition = step * (-.5f * (_items.Length - 1));
+            for (int i = 0; i < _items.Length; i++)
+            {
+                _items[i].localPosition = startingPosition + step * i;
+            }
+        }
+    }
+}
