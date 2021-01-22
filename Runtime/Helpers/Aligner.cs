0bb117b1da519f4f81fabafd46cc39558bb2e19c
Ilia Petrov <mfunc@outlook.com>
2021-01-22 17:34:05 +0300

    1.3.15: Added ability to init Aligner from a prefab.

--- Runtime/Helpers/Aligner.cs
+++ Runtime/Helpers/Aligner.cs
@@ -4,11 +4,30 @@ namespace Mane.Extensions
 {
     public class Aligner : MonoBehaviour
     {
+        [Header("Init with")]
+        [SerializeField] private Transform _itemPrefab;
+        [SerializeField] private int _multiplications;
+        
+        [Header("Preset")]
         [SerializeField] private float _margin = 1f;
         [SerializeField] private Vector3 _axis = Vector3.right;
         [SerializeField] private Transform[] _items;
 
         private void OnValidate()
+        {
+            Align();
+        }
+
+        private void Start()
+        {
+            if (_items.Length > 0 || _itemPrefab == null || _multiplications <= 0) return;
+
+            _items = new Transform[_multiplications];
+            _items.FillWith(i => Instantiate(_itemPrefab, transform));
+            Align();
+        }
+
+        private void Align()
         {
             Vector3 step = _axis * _margin;
             Vector3 startingPosition = step * (-.5f * (_items.Length - 1));
