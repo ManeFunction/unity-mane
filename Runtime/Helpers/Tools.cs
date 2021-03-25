386acb755d4647c7b35914f652bcb74cf09f0be2
Mane Function <mfunc@outlook.com>
2021-03-25 19:16:33 +0300

    1.4.2: Added Collection.IsNullOrEmpty ; Renamed Tools.GetEnumValues

--- Runtime/Helpers/Tools.cs
+++ Runtime/Helpers/Tools.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 
 namespace Mane.Extensions
 {
-    public static class Tools
+    public static class Enum
     {
-        public static IEnumerable<T> GetEnumValues<T>()
+        public static IEnumerable<T> GetValues<T>()
         {
-            return Enum.GetValues(typeof(T)).Cast<T>();
+            return System.Enum.GetValues(typeof(T)).Cast<T>();
+        }
+    }
+
+
+    public static class Collection
+    {
+        public static bool IsNullOrEmpty<T>(IEnumerable<T> collection)
+        {
+            return collection == null || !collection.Any();
         }
     }
 }
